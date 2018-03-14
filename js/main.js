56877cd50440f3a79ccf5dffb4d5d9d297088f64
Dean Nabee windows8 <suprnatl@outlook.om>
2018-03-14 15:14:15 +0400

    added form validation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+'use strict';
 // Sticky menu on scroll
 const heroImg = document.querySelector('.hero-img');
 const menuBar = document.querySelector("#main-menu");
@@ -48,6 +48,86 @@ function toggleMenu() {
 		burgerMenu.classList.remove('open');
 	}
 	
+}
+
+
+// Form validation
+const form = document.querySelector('form[name="contact-form"]');
+const inputs = form.querySelectorAll('[name]');
+
+//console.log(inputs);
+
+// set the Date input value programmatically
+for(let i = 0; i < inputs.length; i++) {
+	if(inputs[i].name == 'date') {
+		inputs[i].value = today();
+	}
+}
+
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
+
+	for(let i = 0; i < inputs.length; i++) {
+
+		let message = createAnElement('b', inputs[i].name + ' is empty');
+		message.classList.add('message'); 
+
+		// skip the loop if the input are date or submit
+		if(inputs[i].name == 'date' || inputs[i].name == 'submit') {
+			continue;
+		}
+
+		if( checkForEmpty(inputs[i].value) ) {
+			// check if the sibling element is
+			// a <b> tag by comparing its localName
+			if(inputs[i].nextSibling.localName !== "b") {
+				inputs[i].parentNode.insertBefore(message, inputs[i].nextSibling);
+			}
+
+		} else {
+			if(inputs[i].nextSibling.localName === "b") {
+				inputs[i].parentNode.removeChild(inputs[i].nextSibling);
+			}
+		}
+	}
+
+});
+
+
+// Function today
+// return the current date
+function today() {
+	const d = new Date();
+	let date = d.getDate(),
+		month = d.getMonth() + 1,
+		year = d.getFullYear();
+
+	if(date < 10) {
+		date = '0' + date;
+	}
+
+	if(month < 10) {
+		month = '0' + month;
+	}
+
+	return date + '/' + month + '/' + year;
+}
+
+// check if input is empty
+function checkForEmpty(input) {
+	if(input === "") {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// create a new element
+function createAnElement(element, text) {
+	const newEl = document.createElement(element);
+	newEl.textContent = text;
+
+	return newEl;
 }
 
 
